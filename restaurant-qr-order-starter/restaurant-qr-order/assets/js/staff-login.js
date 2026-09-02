48665deaa123b4430241ec5e91a111628ced6e9e
MojiMacha <mothegaming.work@gmail.com>
2026-09-02 09:37:05 +0700

    Delete restaurant-qr-order-starter/restaurant-qr-order directory

--- restaurant-qr-order-starter/restaurant-qr-order/assets/js/staff-login.js
+++
@@ -1,6 +0,0 @@
-import { auth } from "./firebase.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-document.getElementById("loginBtn").onclick=async()=>{
- try{await signInWithEmailAndPassword(auth,email.value,password.value);location.href="dashboard.html";}
- catch(e){document.getElementById("error").textContent="เข้าสู่ระบบไม่สำเร็จ: "+e.message;}
-};
